e6c08e1f0c5117f759ba6dc2a11ed0c0a024ea9f
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2023-11-21 20:38:35 +0000

    Nettoyage startPanel

--- src/rpg/ui/MenuStartPanel.java
+++ src/rpg/ui/MenuStartPanel.java
@@ -13,26 +13,24 @@ public class MenuStartPanel extends JPanel {
 
     private GameInputs gameInputs;
 
+    // Composants de l'interface utilisateur
     private JLabel title;
-
     private JLabel pseudoLabel;
     private JTextField pseudoField;
+    private JRadioButton radioMage, radioChevalier, radioArcher;
 
-    private JRadioButton radioMage;
-    private JRadioButton radioChevalier;
-    private JRadioButton radioArcher;
-
-    private JButton okButton;
-
+    // Constructeur
     public MenuStartPanel(GameInputs gameInputs) {
         this.gameInputs = gameInputs;
         initComponents();
     }
 
+    // Méthode pour définir le nom du joueur
     private void setPlayerName(String name) {
         this.gameInputs.setPlayerName(name);
     }
 
+    // Méthode pour définir la classe du joueur
     private void setPlayerCast() {
         PlayerCast playerClass = null;
         if (this.radioMage.isSelected()) {
@@ -43,92 +41,72 @@ public class MenuStartPanel extends JPanel {
             playerClass = PlayerCast.CHEVALIER;
         }
         this.gameInputs.setPlayerCast(playerClass);
-
     }
 
+    // Initialisation des composants de l'interface utilisateur
     private void initComponents() {
-        // define components
-        this.title = new JLabel("Bienvenue dans votre monde");
-        this.pseudoLabel = new JLabel("Choisissez votre nom : ");
-        this.pseudoField = new JTextField();
+        // Initialisation des composants
+        title = new JLabel("Bienvenue dans votre monde");
+        pseudoLabel = new JLabel("Choisissez votre nom : ");
+        pseudoField = new JTextField();
+        radioMage = new JRadioButton("MAGE");
+        radioChevalier = new JRadioButton("CHEVALIER");
+        radioArcher = new JRadioButton("ARCHER");
 
-        this.radioMage = new JRadioButton("MAGE");
-        this.radioChevalier = new JRadioButton("CHEVALIER");
-        this.radioArcher = new JRadioButton("ARCHER");
-
-        // define panel layout
-        this.setBorder(new EmptyBorder(50, 50, 50, 50));
+        // Configuration du layout
+        setBorder(new EmptyBorder(20, 20, 20, 20)); // 20px de marge tout autour
         GridBagLayout gridbag = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
-        /* GridBagConstraints parameters :
-            int gridx,
-            int gridy,
-            int gridwidth,
-            int gridheight,
-            double weightx,
-            double weighty,
-            int anchor,
-            int fill,
-            java.awt.Insets insets,
-            int ipadx,
-            int ipady
-         */
-        this.setLayout(gridbag);
-        gbc.fill = GridBagConstraints.BOTH;
+        setLayout(gridbag);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        // add components to panel
-        // title
-        this.title.setFont(new Font("Segoe UI", Font.BOLD, 30));
-        gbc.gridwidth = 3;  // 3 columns wide
+        // Ajout du titre
+        title.setFont(new Font("Segoe UI", Font.BOLD, 30));
+        gbc.gridwidth = 3;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gridbag.setConstraints(this.title, gbc);
-        this.add(this.title);
+        add(title, gbc);
 
-
-        // player classes radio buttons
-        // mage
-        this.radioMage.setSelected(true);
+        // Configuration pour répartir les boutons radio uniformément
+        gbc.weightx = 1.0; // Distribue l'espace uniformément entre les éléments
+        gbc.gridwidth = 1;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = 0.0;
-        gbc.weighty = 0.0;
-        gbc.insets = new Insets(20, 0, 20, 0);
-        gridbag.setConstraints(this.radioMage, gbc);
-        this.add(this.radioMage);
-        // elf
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(20, 0, 20, 0);
-        gridbag.setConstraints(this.radioArcher, gbc);
-        this.add(this.radioArcher);
-        // warrior
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(20, 0, 20, 0);
-        gridbag.setConstraints(this.radioChevalier, gbc);
-        this.add(this.radioChevalier);
+        add(radioMage, gbc);
 
-        // pseudo of the player
-        this.add(this.pseudoLabel,
-                new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
-                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                        new Insets(50, 0, 30, 0), 0, 0));
-        this.pseudoField.addKeyListener(new KeyAdapter() {
+        gbc.gridx = 1;
+        add(radioArcher, gbc);
+
+        gbc.gridx = 2;
+        add(radioChevalier, gbc);
+
+        // Ajout du champ de saisie pour le nom du joueur
+        gbc.weightx = 0.0; // Réinitialisation pour les autres composants
+        gbc.gridwidth = 3;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        add(pseudoLabel, gbc);
+
+        gbc.gridy = 3;
+        pseudoField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    JTextField textField = (JTextField) e.getSource();
-                    setPlayerName(textField.getText());
+                    setPlayerName(pseudoField.getText());
                     setPlayerCast();
                 }
             }
         });
-        this.add(this.pseudoField,
-                new GridBagConstraints(1, 2, 3, 1, 0.0, 0.0,
-                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                        new Insets(50, 0, 30, 0), 0, 0));
+        add(pseudoField, gbc);
+    }
+
+
+    // Méthode auxiliaire pour ajouter des composants au GridBagLayout
+    private void addToGridBag(Component component, int x, int y, GridBagLayout gridBag, GridBagConstraints gbc) {
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.insets = new Insets(20, 0, 20, 0);
+        gridBag.setConstraints(component, gbc);
+        add(component);
     }
 }
